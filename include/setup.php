44647d55caf4962edf8e9cbbc73516179772f4d7
Jonas Odencrants <jonas.odencrants@redpill-linpro.com>
2012-03-29 20:18:11 +0200

    Renamed the entrypoint to setup

--- include/setup.php
+++ include/setup.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'include/DataBaseMocker.php';
+require_once 'include/database/DataBaseManager.php';
 require_once 'include/mvc/Application.php';
 require_once 'include/mvc/Controller.php';
 require_once 'include/mvc/View.php';
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once('include/entryPoint.php');
+require_once('include/setup.php');
 
 $app = new Application();
 $app->execute();
