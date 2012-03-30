b5db21d2e3d84fd05a1a04bcfa83cd6979fbabff
Jonas Odencrants <jonas.odencrants@redpill-linpro.com>
2012-03-30 02:29:45 +0200

    Added GPL 3

--- include/mvc/View.php
+++ include/mvc/View.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * Copyright 2011 Redpill Linpro AB.
+ * 
+ * This file is part of Redcruiter.
+ *
+ * Redcruiter is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Redcruiter is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Redcruiter. If not, see <http://www.gnu.org/licenses/>.
+ */
 require_once 'include/smarty/libs/Smarty.class.php';
 
 class View
